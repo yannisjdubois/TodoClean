4ff567ce68cc38e124c2790e7d3c0eaed136c85c
y2Dev <dub-y@tuta.io>
2022-07-20 12:17:43 -0400

    ajout d'un tableau contenant les tâches de la todolist

--- components/Todolist.js
+++ components/Todolist.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View,  } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
 import React, { useState } from 'react'
 import { Input, Icon } from '@rneui/themed';
 
