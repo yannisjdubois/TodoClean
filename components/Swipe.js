daf6e3106c049eaf889e028f67ffac98ae4f74b1
y2Dev <dub-y@tuta.io>
2022-07-25 08:54:15 -0400

    Amélioration de la mise en forme du code

--- components/Swipe.js
+++ components/Swipe.js
@@ -1,31 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Icon, ListItem, Button } from '@rneui/themed';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {Icon, ListItem, Button} from '@rneui/themed';
 
-const Swipe = ({tache,id, suppCallBack})=>{
-    return (
-        <ListItem.Swipeable
+const Swipe = ({tache, id, suppCallBack}) => {
+  return (
+    <ListItem.Swipeable
+      rightContent={supprimer => (
+        <Button
+          title="Delete"
+          onPress={() => suppCallBack(id)}
+          icon={{name: 'delete', color: 'white'}}
+          buttonStyle={{minHeight: '100%', backgroundColor: 'red'}}
+        />
+      )}>
+      <Icon name="bookmark" />
+      <ListItem.Content>
+        <ListItem.Title>{tache}</ListItem.Title>
+      </ListItem.Content>
+      <ListItem.Chevron />
+    </ListItem.Swipeable>
+  );
+};
 
-        rightContent={(supprimer) => (
-            
-            <Button
-            title="Delete"
-            onPress={() => suppCallBack(id)}
-            icon={{ name: 'delete', color: 'white' }}
-            buttonStyle={{ minHeight: '100%', backgroundColor: 'red' }}
-            />
-        )}
-        >
-        <Icon name="bookmark" />
-        <ListItem.Content>
-            <ListItem.Title>{tache}</ListItem.Title>
-        </ListItem.Content>
-        <ListItem.Chevron />
-        </ListItem.Swipeable>
-    )
-}
+export default Swipe;
 
-
-export default Swipe
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- components/Todolist.js
+++ components/Todolist.js
@@ -1,87 +1,60 @@
-import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
-import React, { useState } from 'react';
-import { Input, Icon, ListItem, Button, SpeedDial } from '@rneui/themed';
+import {StyleSheet, Text, View, FlatList, SafeAreaView} from 'react-native';
+import React, {useState} from 'react';
+import {Input, Icon, ListItem, Button, SpeedDial} from '@rneui/themed';
 import Swipe from './Swipe';
 import DialogInput from 'react-native-dialog-input';
 
-
 const initTask = [
-    {id:1, tache:"Réussir"},
-    {id:2, tache:"Faire le meilleur"}
-]
+  {id: 1, tache: 'Réussir'},
+  {id: 2, tache: 'Faire le meilleur'},
+];
 
 const Todolist = () => {
-    const [getText, setText] = useState("");
+  const [getText, setText] = useState('');
 
-    const [getTask, setTask] = useState(initTask);
-    
+  const [getTask, setTask] = useState(initTask);
 
-    // Détection des changement sur mon input
-    const textChange = (textValue) => {
-        console.log("Changement du texte", textValue)
+  // Détection des changement sur mon input
+  const textChange = textValue => {
+    console.log('Changement du texte', textValue);
 
-        // Mise à jour de la valeur de mon texte
-        setText(textValue)
-    }
+    // Mise à jour de la valeur de mon texte
+    setText(textValue);
+  };
 
+  const ajouter = textValue => {
+    console.log('Voir les ajouts', textValue);
 
+    // Quand ...getTask est en bas, le plus récent se retrouve en haut de la liste
+    setTask([{id: getTask.length + 1, tache: textValue}, ...getTask]);
 
-    const ajouter = (textValue) => {
-        
-        console.log("Voir les ajouts", textValue)
+    // Remise à zero de mon formulaire
+    setText('');
 
-        // Quand ...getTask est en bas, le plus récent se retrouve en haut de la liste
-        setTask([
-            {id:getTask.length+1,
-            tache: textValue
-        },
-        ...getTask,
-    ])
+    setOpenDialog(!openDialog);
+  };
 
-        // Remise à zero de mon formulaire
-        setText("")
+  const supprimer = id => {
+    console.log('Les suppressions', id);
 
-        setOpenDialog(!openDialog)
+    // Variable temporaire pour filtrer tous les id différents de l'id sélectionné
+    const filterTask = getTask.filter(item => item.id != id);
 
+    // Permet la mise à niveau du filterTask
+    setTask(filterTask);
+  };
 
-    }
+  const [open, setOpen] = React.useState(false);
 
-
-
-
-    const supprimer = (id) => {
-        console.log("Les suppressions", id)
-
-        // Variable temporaire pour filtrer tous les id différents de l'id sélectionné
-        const filterTask = getTask.filter (item => item.id != id)
-
-        // Permet la mise à niveau du filterTask
-        setTask (filterTask)
-    }
-
-    
-    const [open, setOpen] = React.useState(false);
-
-    const [openDialog, setOpenDialog] = React.useState(false);
-
-    
+  const [openDialog, setOpenDialog] = React.useState(false);
 
   return (
-
     <SafeAreaView>
-        <FlatList
+      <FlatList
         data={getTask}
-        renderItem={
-            ({item}) =>
-
-            <Swipe 
-            tache={item.tache}
-            id={item.id}
-            suppCallBack = {supprimer}
-            />
-
-        }
-
+        renderItem={({item}) => (
+          <Swipe tache={item.tache} id={item.id} suppCallBack={supprimer} />
+        )}
 
         // Message personnalisé si liste de tâche vide
         // ListEmptyComponent={() => (
@@ -89,40 +62,37 @@ const Todolist = () => {
         // )}
 
         // keyExtractor={item => item.id}
-        />
+      />
 
-        <DialogInput isDialogVisible={openDialog}
-            title={"DialogInput 1"}
-            message={"Message for DialogInput #1"}
-            hintInput ={"HINT INPUT"}
-            submitInput={ (inputText) => {ajouter(inputText)} }
-            closeDialog={ () => {setOpenDialog(!openDialog)}}>
-        </DialogInput>
+      <DialogInput
+        isDialogVisible={openDialog}
+        title={'DialogInput 1'}
+        message={'Message for DialogInput #1'}
+        hintInput={'HINT INPUT'}
+        submitInput={inputText => {
+          ajouter(inputText);
+        }}
+        closeDialog={() => {
+          setOpenDialog(!openDialog);
+        }}></DialogInput>
 
-        <SpeedDial
-                isOpen={open}
-                icon={{ name: 'edit', color: '#fff' }}
-                openIcon={{ name: 'close', color: '#fff' }}
-                onOpen={() => setOpenDialog(!openDialog)}
-                onClose={() => setOpenDialog(!openDialog)}
-                style={{height:750}}
-                >
+      <SpeedDial
+        isOpen={open}
+        icon={{name: 'edit', color: '#fff'}}
+        openIcon={{name: 'close', color: '#fff'}}
+        onOpen={() => setOpenDialog(!openDialog)}
+        onClose={() => setOpenDialog(!openDialog)}
+        style={{height: 750}}>
         <SpeedDial.Action
-        icon={{ name: 'add', color: '#fff' }}
-        title="Add"
-        onPress={() => setOpenDialog(!openDialog)}
+          icon={{name: 'add', color: '#fff'}}
+          title="Add"
+          onPress={() => setOpenDialog(!openDialog)}
         />
-            
-        </SpeedDial>
-
+      </SpeedDial>
     </SafeAreaView>
+  );
+};
 
-    
+export default Todolist;
 
-        
-  )
-}
-
-export default Todolist
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
